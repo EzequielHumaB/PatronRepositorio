a08c426346604e7d2dd3114f53915b2379c6b146
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-20 14:27:45 -0400

    Creacion de las entidades

---
+++ PatronRepositorioConPruebas/Entidades/DetalleVentas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace PatronRepositorioConPruebas.Entidades
+{
+   public class DetalleVentas
+    {
+        public int DetalleVentasId { get; set; }
+        public int Ventas_VentasId { get; set; }
+        public int Productos_ProductoId { get; set; }
+        public double Unidades { get; set; }
+        public double CostoUnidad { get; set; }
+        public double DescuentoUnidad { get; set; }
+        public double Total { get; set; }
+
+        public DetalleVentas()
+        {
+            DetalleVentasId = 0;
+            Ventas_VentasId = 0;
+            Productos_ProductoId = 0;
+            Unidades = 0.0d;
+            CostoUnidad = 0.0d;
+            DescuentoUnidad = 0.0d;
+            Total = 0.0d;
+        }
+
+    }
+}
---
+++ PatronRepositorioConPruebas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatronRepositorioConPruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
